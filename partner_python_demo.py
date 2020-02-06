08020ee5a82899552f3ae43c5991de5b8b137564
Jolly23 <zhaolei@pm.me>
2020-02-06 01:16:24 -0800

    Update partner_python_demo.py

--- partner_python_demo.py
+++ partner_python_demo.py
@@ -5,14 +5,18 @@ import requests
 import hashlib
 import hmac
 import time
-import random
-import string
-import math
 
 PARTNER_ACCOUNT_API_KEY = "USEAPIKEYASMYID"
-PARTNER_ACCOUNT_SECRET = "S1XP1e3UZj6A7H5fATj0jNhqPtmdSJYdInClVN65XAbvqqMKjVHjA7PZj4W12oep"
+PARTNER_ACCOUNT_SECRET = "THISISTHESECRET"
 
 BASE_URL = "https://mock-api.roostoo.com"
+
+
+def generate_signature(params, secret=PARTNER_ACCOUNT_SECRET):
+    query_string = '&'.join(["{}={}".format(k, params[k]) for k in sorted(params.keys())])
+    us = secret.encode('utf-8')
+    m = hmac.new(us, query_string.encode('utf-8'), hashlib.sha256)
+    return m.hexdigest()
 
 
 def gen_join_code_for_partner():
@@ -63,7 +67,6 @@ def leader_board_for_partner():
         }
     )
     print r.status_code, r.text
-    print r.headers
 
 
 if __name__ == '__main__':
